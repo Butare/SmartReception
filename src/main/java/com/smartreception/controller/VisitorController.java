7705811cbe2fade531da8083098a18ffe8908605
Butare <bjimsons@gmail.com>
2019-03-17 22:36:01 +0900

    change - response status

--- src/main/java/com/smartreception/controller/VisitorController.java
+++ src/main/java/com/smartreception/controller/VisitorController.java
@@ -43,7 +43,7 @@ public class VisitorController {
   }
   
   @PutMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @ResponseStatus(HttpStatus.OK)
   public Visitor update(@PathVariable("id") long id, @Valid Visitor visitor) {
 	visitorService.update(id, visitor);
 	return visitorService.getVisitorById(id);
--- src/test/java/com/smartreception/controller/VisitorControllerTest.java
+++ src/test/java/com/smartreception/controller/VisitorControllerTest.java
@@ -90,7 +90,7 @@ public class VisitorControllerTest {
   }
   
   @Test
-  public void testPutURIShouldReturnStatusNoContent() throws Exception {
+  public void testPutURIShouldReturnStatusOK() throws Exception {
 	// data
     long id = 1L;
 	Visitor fakeVisitor = createVisitor(id);
@@ -99,7 +99,7 @@ public class VisitorControllerTest {
 	mockMvc.perform(put("/visitors/{id}", id)
 		.contentType(MediaType.APPLICATION_JSON)
 		.content(TestUtils.convertObjectToStringBytes(fakeVisitor)))
-		.andExpect(status().isNoContent());
+		.andExpect(status().isOk());
   }
   
   @Test
